00567292a4b7a415ed593b44abd5e1d8cd9dbce4
Sheepnore <a83840066@gmail.com>
2025-02-24 17:26:23 +0800

    feat: install MUI and add RefreshBtn

--- src/ui/MainPage/MainPage.jsx
+++ src/ui/MainPage/MainPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import RefreshBtn from "./RefreshBtn";
 
 const MainPage = () => {
   return (
     <section>
-      <h1 className="text-2xl">All Posts</h1>
+      <div className="flex justify-between">
+        <h1 className="text-2xl">All Posts</h1>
+        <RefreshBtn />
+      </div>
       <table></table>
     </section>
   );
---
+++ src/ui/MainPage/RefreshBtn.jsx
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+
+export default function RefreshBtn() {
+  return (
+    <Button variant="contained" color="primary" size="large">
+      Refresh
+    </Button>
+  );
+}
