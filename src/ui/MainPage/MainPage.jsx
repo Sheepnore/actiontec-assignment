1d37a553921cd26a4ff6a8edf1ba62992a4bc891
Sheepnore <a83840066@gmail.com>
2025-02-25 16:58:23 +0800

    feat: add Link to navigate to PostDetailsPage.
    Render posts based on the fetched data.

--- src/ui/MainPage/MainPage.jsx
+++ src/ui/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import RefreshBtn from "./RefreshBtn";
 import fetchPostData from "../../data/fetchPostData";
+import { Link } from "react-router";
 
 const MainPage = () => {
   const [postData, setPostData] = useState([]);
@@ -15,6 +16,7 @@ const MainPage = () => {
   }, []);
 
   console.log(postData);
+
   return (
     <section className="grid gap-y-8">
       <div className="flex justify-between">
@@ -22,34 +24,27 @@ const MainPage = () => {
         <RefreshBtn />
       </div>
       <table className="w-full">
-        <thead className="bg-gray-50 border">
-          <tr>
+        <thead className="bg-gray-50">
+          <tr className="border [&>th]:p-4 text-left">
             <th scope="col">Id</th>
             <th scope="col">Title</th>
             <th scope="col">User</th>
           </tr>
         </thead>
         <tbody className="bg-white">
-          <tr className="border">
-            <th scope="row">1</th>
-            <td>Post 1</td>
-            <td>22</td>
-          </tr>
-          <tr className="border">
-            <th scope="row">2</th>
-            <td>Post 2</td>
-            <td>45</td>
-          </tr>
-          <tr className="border">
-            <th scope="row">3</th>
-            <td>Post 3</td>
-            <td>29</td>
-          </tr>
-          <tr className="border">
-            <th scope="row">4</th>
-            <td>Post 4</td>
-            <td>36</td>
-          </tr>
+          {postData.map((post) => {
+            return (
+              <tr key={post.id} className="border [&>th]:p-4 text-left">
+                <th scope="col">{post.id}</th>
+                <th scope="col">
+                  <Link to="/postDetailsPage" className="hover:underline">
+                    {post.title}
+                  </Link>
+                </th>
+                <th scope="col">{post.userId}</th>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </section>
